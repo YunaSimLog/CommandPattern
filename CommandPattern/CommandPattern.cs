008d1dcf2a9251a923b766b8f316a7b49cfade4b
yunasim <ynsim@solidit.co.kr>
2023-06-21 23:46:28 +0900

    #😎update: 복잡한 형태의 커맨드 패턴 구현

--- CommandPattern/CommandPattern.cs
+++ CommandPattern/CommandPattern.cs
@@ -33,19 +33,48 @@ namespace CommandPattern
     }
 
     /// <summary>
+    /// 복잡한 형태의 작업 구현
+    /// Receiver 객체
+    /// </summary>
+    class ComplexCommand : ICommand
+    {
+        Receiver m_Receiver;
+
+        // Receiver 메소드 실행에 필요한 컨텍스트 테이터
+        string m_strValue1;
+        string m_strValue2;
+
+        // Receiver의 객체 또는 컨텍스트 데이터를 받을 수 있다.
+        public ComplexCommand(Receiver receiver, string strValue1, string strValue2)
+        {
+            m_Receiver = receiver;
+            m_strValue1 = strValue1;
+            m_strValue2 = strValue2;
+        }
+
+        // 명령을 Receiver의 모든 메서드에 위임
+        public void Execute()
+        {
+            Console.WriteLine("ComplexCommand: Receiver 개체에서 수행");
+            this.m_Receiver.DoSomething(m_strValue1);
+            this.m_Receiver.DoSomethingElse(m_strValue2);
+        }
+    }
+
+    /// <summary>
     /// 수행과 관련된 모든 종류의 작업을 수행
     /// 수신기 역활을 하는 클래스
     /// </summary>
     class Receiver
     {
-        public void DoSomething(string strValue)
+        public void DoSomething(string strValue1)
         {
-            Console.WriteLine($"Receiver: DoSomething() ({strValue}.)");
+            Console.WriteLine($"Receiver: DoSomething() ({strValue1}.)");
         }
 
-        public void DoSomethingElse(string strValue)
+        public void DoSomethingElse(string strValue2)
         {
-            Console.WriteLine($"Receiver: DoSomethingElse() ({strValue}.)");
+            Console.WriteLine($"Receiver: DoSomethingElse() ({strValue2}.)");
         }
     }
 
@@ -61,13 +90,13 @@ namespace CommandPattern
 
         public void SetOnFinish(ICommand command)
         {
-            m_OnFinish=command;
+            m_OnFinish = command;
         }
 
         public void DoSomethingImportant()
         {
             Console.WriteLine("Invoker: 이 행동 시작전에 일이 완료되야 할 사항이 있는가?");
-            if(m_OnStart is ICommand)
+            if (m_OnStart is ICommand)
             {
                 m_OnStart.Execute();
             }
@@ -75,9 +104,9 @@ namespace CommandPattern
             Console.WriteLine("Invoker: ...중요한 작업이 진행중...");
 
             Console.WriteLine("Invoker: 이 행동이 끝난 후 진행해야 할 사항이 있는가?");
-            if( m_OnFinish is ICommand)
+            if (m_OnFinish is ICommand)
             {
-                m_OnFinish.Execute(); 
+                m_OnFinish.Execute();
             }
         }
     }
--- CommandPattern/Program.cs
+++ CommandPattern/Program.cs
@@ -10,11 +10,16 @@ namespace CommandPattern
     {
         static void Main(string[] args)
         {
-            Invoker invoker = new Invoker();
-            invoker.SetOnStart(new SimpleCommand("Say Hi!"));
-            invoker.SetOnFinish(new SimpleCommand("Say Bye!"));
-            invoker.DoSomethingImportant();
+            //Invoker invoker = new Invoker();
+            //invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+            //invoker.SetOnFinish(new SimpleCommand("Say Bye!"));
+            //invoker.DoSomethingImportant();
 
+            Invoker invoker2 = new Invoker();
+            Receiver receiver = new Receiver();
+            invoker2.SetOnStart(new ComplexCommand(receiver, "Val1", "Val2"));
+            invoker2.SetOnFinish(new ComplexCommand(receiver, "Val3", "Val4"));
+            invoker2.DoSomethingImportant();
         }
     }
 }
